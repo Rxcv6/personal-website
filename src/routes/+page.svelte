1dd3fbd9df8fc4843f4ba06254045eeaf235ec8f
Rxcv6 <161628926+Rxcv6@users.noreply.github.com>
2024-07-31 10:48:16 +0300

    blue card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
-
 </script>
 
-<div class="bg-secondary min-w-3 min-h-3">
+<div class=" flex justify-end">
+  <div class="bg-secondary min-h-[calc(100vh-6.5rem)] min-w-[60vw] relative flex items-center">
+    <div class="bg-blue-500 w-[20rem] h-[30rem] absolute start-10 -translate-x-full">
 
+    </div>
+
+  </div>
 </div>
