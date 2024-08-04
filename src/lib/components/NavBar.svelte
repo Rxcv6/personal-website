070fe5da5bdb6485dbe83b5d03044911bd36633e
Rxcv6 <161628926+Rxcv6@users.noreply.github.com>
2024-08-04 15:38:48 +0300

    card done

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -6,7 +6,6 @@
   import ThemeSwitch from "$lib/components/ThemSwitch.svelte";
   import ThemSwitch from "$lib/components/ThemSwitch.svelte";
 
-
   const links = [
     {
       text: "About Me",
@@ -28,17 +27,22 @@
   let isOpen = false;
 </script>
 
-<div class=" bg-secondary text-secondary-foreground  py-8 px-4">
+<div class=" bg-secondary text-secondary-foreground py-8 px-4">
   <nav class="max-w-screen-2xl flex justify-between items-center mx-auto">
     <div class="flex gap-2 items-center">
       <div class="bg-primary h-6 w-6 me-3"></div>
-      <div class=" flex items-end gap-2">
-        <span class="font-bold text-3xl">Badr Mousa Al yasi </span>
-        <span class="text-xl uppercase"> / Computer science</span>
-      </div>
+      <a href="/">
+        <div class=" flex items-end gap-2">
+          <span class="font-bold text-3xl">Badr Al yasi </span>
+
+          <span class="text-xl uppercase hidden sm:flex">
+            / Computer science</span
+          >
+        </div>
+      </a>
     </div>
     <div class="flex">
-      <ThemSwitch/>
+      <ThemSwitch />
       <div class="uppercase hidden lg:flex">
         {#each links as link}
           <Button
@@ -50,16 +54,16 @@
       </div>
       <Sheet.Root bind:open={isOpen}>
         <Sheet.Trigger class="flex lg:hidden">
-          <Button  size="icon" variant="ghost"
-            ><MenuIcon></MenuIcon></Button
+          <Button size="icon" variant="ghost"><MenuIcon></MenuIcon></Button
           ></Sheet.Trigger
         >
         <Sheet.Content class="flex flex-col justify-center">
           {#each links as link}
-            <Button  on:click={() => isOpen = false}
+            <Button
+              on:click={() => (isOpen = false)}
               class=" {$page.url.pathname == link.href && 'text-primary'}"
               href={link.href}
-              variant="link" >{link.text}</Button
+              variant="link">{link.text}</Button
             >
           {/each}
         </Sheet.Content>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,63 @@
 <script lang="ts">
+  import Button from "$lib/components/ui/button/button.svelte";
+  import X from "$lib/components/x.svelte";
+  import Instag from "$lib/components/instag.svelte";
 </script>
 
 <div class=" flex justify-end">
-  <div class="bg-secondary min-h-[calc(100vh-6.5rem)] min-w-[60vw] relative flex items-center">
-    <div class="bg-blue-500 w-[20rem] h-[30rem] absolute start-10 -translate-x-full">
-
+  <div
+    class="bg-secondary relative min-h-[40vh] min-w-full lg:min-h-[calc(100vh-6.5rem)] lg:min-w-[60vw] items-center flex justify-center text-secondary-foreground pb-32"
+  >
+    <div
+      class="bg-[#f4ece6] dark:bg-[#303030]  shadow-[0_30px_70px_-15px_rgba(0,0,0,0.6)] w-[20rem]  translate-y-[85%] lg:translate-y-1/4 absolute lg:start-10 lg:-translate-x-full flex flex-col items-center justify-between pt-8 gap-3 self-start "
+    >
+      <img
+        src="/avater.jpg"
+        class="w-60 h-56 grayscale rounded-full"
+        alt="صورة شخصية"
+      />
+      <div class=" flex flex-col items-center gap-6  dark:text-white">
+        <h1 class="font-bold text-4xl">Badr</h1>
+        <hr class="w-20 border-primary border-2" />
+        <h2 class="text-2xl">Programming</h2>
+      </div>
+      <div
+        class="bg-secondary w-full flex justify-center p-2 mt-10 text-secondary-foreground"
+      >
+        <Button
+          size="icon"
+          variant="ghost"
+          href="https://x.com/_wetq"
+          target="_blank"
+        >
+          <X></X>
+        </Button>
+        <Button
+          size="icon"
+          variant="ghost"
+          href="https://www.instagram.com/1iav1/"
+          target="_blank"
+        >
+          <Instag></Instag>
+        </Button>
+      </div>
     </div>
-
+    <div class="mx-10 max-w-[30rem] flex flex-col gap-8">
+      <h1 class="text-8xl font-bold">Hello</h1>
+      <h4>here's who I am & what I do</h4>
+      <div class="flex gap-2">
+        <Button class="w-32" href="/resume">Resume</Button>
+        <Button variant="ghost"  class="w-32 border" href="/projects">Projects</Button>
+      </div>
+      <div>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+          suscipit veritatis natus autem architecto? Mollitia doloremque, non
+          beatae minus ea voluptatibus eum, quos aliquid enim ab sequi
+          necessitatibus placeat aliquam.
+        </p>
+      </div>
+    </div>
   </div>
+  
 </div>
